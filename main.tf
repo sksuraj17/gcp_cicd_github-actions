2e5fdedab4227d93e7741a4809d038c54fb060cc
Suraj <surajsk2002@gmail.com>
2025-03-14 18:33:50 +0000

    cicd -- check-07

--- main.tf
+++ main.tf
@@ -3,8 +3,42 @@ provider "google" {
   region  = "us-central1"
 }
 
-resource "google_storage_bucket" "my-bucket" {
-  name          = "my-bucket-for-cicd-test-gcp"
+resource "google_compute_instance" "my-instance" {
+  name         = "my-instance-01"
+  machine_type = "e2-micro"
+  zone         = "any"
+
+  boot_disk {
+    initialize_params {
+      image = "debian-cloud/debian-11"
+    }
+  }
+
+  network_interface {
+    network = "default"
+  }
+}
+
+resource "google_storage_bucket" "my-storage-bucket" {
+  name          = "my-storage-bucket-for-testing-cicd"
   location      = "US"
   force_destroy = true
+
+  lifecycle_rule {
+    condition {
+      age = 3
+    }
+    action {
+      type = "Delete"
+    }
+  }
+
+  lifecycle_rule {
+    condition {
+      age = 1
+    }
+    action {
+      type = "AbortIncompleteMultipartUpload"
+    }
+  }
 }
